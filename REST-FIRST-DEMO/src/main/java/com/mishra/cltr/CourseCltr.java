b90eb32d33656a9a329b9b7e556a9b6da282b3b9
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-10 01:04:15 +0530

    @ ResponseBody

--- REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
+++ REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
@@ -1,30 +1,41 @@
 package com.mishra.cltr;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mishra.entity.Course;
+import com.mishra.service.CourseService;
 
 @Controller
-				//here we are doing Class level mapping
+// here we are doing Class level mapping
 @RequestMapping("Course")
 public class CourseCltr {
+	@Autowired
+	CourseService courseService;
 
-				//now if /course url is called then this method is called
-				//if all the method have same requestmapping then it will create ambiguity
-	@RequestMapping
+	/*
+	 * now if /course url is called then this method is called if all the method
+	 * have same requestmapping then it will create ambiguity
+	 */
+	@RequestMapping(method = RequestMethod.GET) // here we are telling to use get method
+
+	/*
+	 * this tag is used to tell that it just a response body not a view to our front
+	 * controller as every string or object is considerd as view
+	 */
+	@ResponseBody
 	public void m1() {
-		System.out.println("----m1");
+		Course course = new Course();
+		course.setCode("S102");
+		course.setTitle("Junit");
+		course.setSubject("BackEnd-Programming");
+		course.setFees(2000);
+		courseService.createNewCourse(course);
 	}
-	
-			//where url Course/add is called then this method handler works
-	@RequestMapping(value = "add")
-	public void m2() {
-		System.out.println("----m2");
-	}
-	@RequestMapping(value = "delete")
-	public void m3() {
-		System.out.println("----m3");
-	}
-	
 
 }
