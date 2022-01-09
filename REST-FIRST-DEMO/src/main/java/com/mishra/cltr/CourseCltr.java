29c3d75c20b4ca6bb140c883f14106afb704134f
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-10 00:25:17 +0530

    class level @request /rewuest mapping

--- REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
+++ REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
@@ -2,12 +2,24 @@ package com.mishra.cltr;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("Course")
 public class CourseCltr {
 
-	@RequestMapping("course")
-	public void Test() {
-		System.out.println("---------");
+	@RequestMapping
+	public void m1() {
+		System.out.println("----m1");
 	}
+	@RequestMapping(value = "add")
+	public void m2() {
+		System.out.println("----m2");
+	}
+	@RequestMapping(value = "delete")
+	public void m3() {
+		System.out.println("----m3");
+	}
+	
+
 }
