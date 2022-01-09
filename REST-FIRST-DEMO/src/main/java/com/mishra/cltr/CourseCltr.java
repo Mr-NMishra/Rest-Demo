673f5d3874d3ddb320cdc3d362aa46b7abb4b7d2
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-10 00:28:30 +0530

    class level @request /request mapping

--- REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
+++ REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+				//here we are doing Class level mapping
 @RequestMapping("Course")
 public class CourseCltr {
 
+				//now if /course url is called then this method is called
+				//if all the method have same requestmapping then it will create ambiguity
 	@RequestMapping
 	public void m1() {
 		System.out.println("----m1");
 	}
+	
+			//where url Course/add is called then this method handler works
 	@RequestMapping(value = "add")
 	public void m2() {
 		System.out.println("----m2");
