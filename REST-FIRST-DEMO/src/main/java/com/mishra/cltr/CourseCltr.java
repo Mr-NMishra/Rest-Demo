4b30e499bdfc1daa172a1aa5e259dd31217f8ee8
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-10 01:10:26 +0530

    @RestCOntroller

--- REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
+++ REST-FIRST-DEMO/src/main/java/com/mishra/cltr/CourseCltr.java
@@ -3,16 +3,22 @@ package com.mishra.cltr;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mishra.entity.Course;
 import com.mishra.service.CourseService;
 
-@Controller
-// here we are doing Class level mapping
+@RestController
+				/* this  @RestController is used to tell our front controller that
+				 * every handler present inside this are Response body don't 
+				 * consider as view*/
+//@Controller
+						// here we are doing Class level mapping
 @RequestMapping("Course")
 public class CourseCltr {
 	@Autowired
@@ -22,19 +28,19 @@ public class CourseCltr {
 	 * now if /course url is called then this method is called if all the method
 	 * have same requestmapping then it will create ambiguity
 	 */
-	@RequestMapping(method = RequestMethod.GET) // here we are telling to use get method
-
+	//@RequestMapping(method = RequestMethod.GET) // here we are telling to use get method
+	@GetMapping
 	/*
 	 * this tag is used to tell that it just a response body not a view to our front
 	 * controller as every string or object is considerd as view
 	 */
-	@ResponseBody
+	//@ResponseBody
 	public void m1() {
 		Course course = new Course();
-		course.setCode("S102");
-		course.setTitle("Junit");
+		course.setCode("S103");
+		course.setTitle("Core");
 		course.setSubject("BackEnd-Programming");
-		course.setFees(2000);
+		course.setFees(6000);
 		courseService.createNewCourse(course);
 	}
 
